044d804e76ddb89a08cc4bfaff9eb197d8ebf93a
Joseph Tang <162398761+mintetang@users.noreply.github.com>
2025-08-11 15:07:10 +0800

    first commit

---
+++ test-site/scripts/main.js
@@ -0,0 +1,38 @@
+// Store a reference to the <h1> in a variable
+//const myHeading = document.querySelector("h1");
+// Update the text content of the <h1>
+//myHeading.textContent = "Hello world!";
+const myImage = document.querySelector("img");
+myImage.addEventListener("click", () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/a1.jpg") {
+    myImage.setAttribute("src", "images/church.jpg");
+  } else {
+    myImage.setAttribute("src", "images/a1.jpg");
+  }
+});
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+function setUserName() {
+  const myName = prompt("Please INPUT your name.");
+  if (myName === null) {
+  console.log("User cancelled the prompt.");
+  // Perform actions for cancellation, e.g., stop further processing
+} else if (myName === "") {
+  console.log("User clicked OK but entered no text.");
+  // Handle empty input
+} else {
+  console.log("User entered: " + myName);
+  // Process the user's input
+}
+  if (!myName) {
+    //setUserName();
+    myHeading.textContent = `No input, please click button again`;
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `My Church is cool, ${myName}`;
+  }
+}
+myButton.onclick = () => {
+  setUserName();
+};
