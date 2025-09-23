047ca9003e54d74b5f89213a8c58888a3cbd3a72
Joseph Tang <162398761+mintetang@users.noreply.github.com>
2025-09-23 10:55:59 +0800

    make 1st roster version

---
+++ test-site/scripts/roster.js
@@ -0,0 +1,56 @@
+async function populate() {
+  const requestURL =
+    //"https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json";
+  "https://mintetang.github.io/web-projects/test-site/scripts/superheros.json";
+  const request = new Request(requestURL);
+
+  const response = await fetch(request);
+  const superHeroes = await response.json();
+
+  populateHeader(superHeroes);
+  populateHeroes(superHeroes);
+}
+function populateHeader(obj) {
+  const header = document.querySelector("header");
+  const myH1 = document.createElement("h1");
+  myH1.textContent = obj.squadName;
+  header.appendChild(myH1);
+
+  const myPara = document.createElement("p");
+  myPara.textContent = `地點: ${obj.homeTown} // 主後: ${obj.formed}`;
+  header.appendChild(myPara);
+}
+function populateHeroes(obj) {
+  const section = document.querySelector("section");
+  const heroes = obj.members;
+
+  for (const hero of heroes) {
+    const myArticle = document.createElement("article");
+    const myH2 = document.createElement("h2");
+    const myPara1 = document.createElement("p");
+    const myPara2 = document.createElement("p");
+    const myPara3 = document.createElement("p");
+    const myList = document.createElement("ul");
+
+    myH2.textContent = hero.name;
+    myPara1.textContent = `職務: ${hero.secretIdentity}`;
+    myPara2.textContent = `年齡: ${hero.age}`;
+    myPara3.textContent = "專長:";
+
+    const superPowers = hero.powers;
+    for (const power of superPowers) {
+      const listItem = document.createElement("li");
+      listItem.textContent = power;
+      myList.appendChild(listItem);
+    }
+
+    myArticle.appendChild(myH2);
+    myArticle.appendChild(myPara1);
+    myArticle.appendChild(myPara2);
+    myArticle.appendChild(myPara3);
+    myArticle.appendChild(myList);
+
+    section.appendChild(myArticle);
+  }
+}
+populate();
